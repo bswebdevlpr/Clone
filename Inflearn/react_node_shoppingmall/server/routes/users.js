8dbe4088225223dfe709aa48a0e2542ae9578164
bswebdvlpr <tfl0324@gachon.ac.kr>
2023-03-01 02:18:40 +0900

    ~ Add to Cart 버튼 만들기 #2

--- Inflearn/react_node_shoppingmall/server/routes/users.js
+++ Inflearn/react_node_shoppingmall/server/routes/users.js
@@ -71,9 +71,46 @@ router.get("/logout", auth, (req, res) => {
 
 router.post("/addToCart", auth, (req, res) => {
   // 먼저 User collection에 해당 유저의 정보 가져오기
-  // 가져온 정보에서 카트에 넣을 상품이 이미 들어가있는지 확인
-  // 상품이 이미 들어가있다면
-  // 상품이 들어가있지 않다면
+  User.findOne({ _id: req.user._id }, (err, userInfo) => {
+    // 가져온 정보에서 카트에 넣을 상품이 이미 들어가있는지 확인
+
+    let duplicate = false;
+    userInfo.cart.forEach((item) => {
+      if (item.id === req.body.productId) duplicate = true;
+    });
+
+    if (duplicate) {
+      // 상품이 이미 들어가있다면
+      User.findOneAndUpdate(
+        { _id: req.user._id, "cart.id": req.body.productId },
+        { $inc: { "cart.$.quantity": 1 } },
+        { new: true }, // 업데이트 된 정보의 결과값을 얻기 위한 옵션
+        (err, userInfo) => {
+          if (err) return res.status(400).json({ success: false, err });
+          res.status(200).send(userInfo.cart);
+        }
+      );
+    } else {
+      // 상품이 들어가있지 않다면
+      User.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          $push: {
+            cart: {
+              id: req.body.productId,
+              quantity: 1,
+              date: Date.now(),
+            },
+          },
+        },
+        { new: true },
+        (err, userInfo) => {
+          if (err) return res.status(400).json({ success: false, err });
+          res.status(200).send(userInfo.cart);
+        }
+      );
+    }
+  });
 });
 
 module.exports = router;
