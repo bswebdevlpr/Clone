0668e9e567f6035a2119a9fce9d4956b2182c5fa
bswebdvlpr <tfl0324@gachon.ac.kr>
2023-03-12 20:20:46 +0900

    Deploy: server (try 25)

--- Inflearn/react_node_shoppingmall/server/index.js
+++ Inflearn/react_node_shoppingmall/server/index.js
@@ -28,7 +28,7 @@ const connect = mongoose
 app.use(
   cors({
     origin: "*", // 출처 허용 옵션
-    // credential: "true", // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+    credential: "true", // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
   })
 );
 
