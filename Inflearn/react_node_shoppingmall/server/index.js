5872e546b2b9dee89a457c71a787a586ce2affd9
bswebdvlpr <tfl0324@gachon.ac.kr>
2023-03-12 19:18:41 +0900

    Deploy: server (try 22)

--- Inflearn/react_node_shoppingmall/server/index.js
+++ Inflearn/react_node_shoppingmall/server/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
-const config = require("../config/key");
+const config = require("./config/key");
 
 // const mongoose = require("mongoose");
 // mongoose
@@ -34,19 +34,18 @@ app.use(
 
 //to not get any deprecation warning or error
 //support parsing of application/x-www-form-urlencoded post data
-app.use("/", express.static("../api"));
 app.use(bodyParser.urlencoded({ extended: true }));
 //to get json data
 // support parsing of application/json type post data
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use("/api/users", require("../routes/users"));
-app.use("/api/product", require("../routes/product"));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/product", require("./routes/product"));
 
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
-app.use("/uploads", express.static("../uploads"));
+app.use("/uploads", express.static("./uploads"));
 
 // // Serve static assets if in production
 // if (process.env.NODE_ENV === "production") {
