b2794988f79e0498db0983dcb5715200f0a21db3
bswebdvlpr <tfl0324@gachon.ac.kr>
2023-05-29 21:47:23 +0900

    ~MVC 패턴, 프로젝트 셋업

--- Inflearn/NestJS_randomChat/src/app.module.ts
+++ Inflearn/NestJS_randomChat/src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 다른 모듈에서 ConfigModule을 가져올 필요가 없어진다.
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure() {
+    const DEBUG = process.env.MODE === 'dev' ? true : false;
+    mongoose.set('debug', DEBUG);
+  }
+}
