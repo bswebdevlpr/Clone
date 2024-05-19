a487b0176876447b2ae563383113d7b83f49e6ad
bswebdevlpr <tfl0324@gachon.ac.kr>
2024-05-19 21:43:32 +0900

    ~3 Authentication

--- NomadCoder/Twitter/src/routes/create-account.tsx
+++ NomadCoder/Twitter/src/routes/create-account.tsx
@@ -1,48 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0px;
-`;
-
-const Title = styled.h1`
-  font-size: 42px;
-`;
-
-const Form = styled.form`
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-
-const Input = styled.div`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100px;
-  font-size: 16px;
-  &[type="submit"] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
-`;
+import { auth } from "../firebase";
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import {
+  Wrapper,
+  Title,
+  Form,
+  Input,
+  Error,
+  Switcher,
+} from "../components/auth-components";
+import GithubButton from "../components/github-btn";
 
 export default function CreateAccount() {
+  const navigate = useNavigate();
+
   const [isLoading, setIsLoading] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -57,15 +31,31 @@ export default function CreateAccount() {
     else if (name === "email") setEmail(value);
     else if (password === "password") setPassword(value);
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setError("");
+
+    if (isLoading || name === "" || email === "" || password === "") return;
+
     try {
-    } catch (e) {}
+      setIsLoading(true);
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      await updateProfile(credentials.user, {
+        displayName: name,
+      });
+      navigate("/");
+    } catch (e) {
+      if (e instanceof FirebaseError) setError(e.message);
+    }
   };
 
   return (
     <Wrapper>
-      <Title>Log into Twitter</Title>
+      <Title>Join Twitter</Title>
       <Form>
         <Input
           onChange={onChange}
@@ -97,6 +87,11 @@ export default function CreateAccount() {
         />
       </Form>
       {error !== "" ? <Error>{error}</Error> : null}
+      <Switcher>
+        Already have an account?
+        <Link to="/login">Log in &rarr;</Link>
+      </Switcher>
+      <GithubButton />
     </Wrapper>
   );
 }
